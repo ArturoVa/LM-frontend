12b52f80cbcaa1b0c37f7fc9a9284de24fa92414
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2024-05-26 03:06:45 -0600

    Se cambio ruta y se agregan mas

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import "./App.css";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Login } from "./Pages/login";
 import { Home } from "./Pages/home";
+import { NewTask } from "./Pages/new-task";
+import { Information } from "./Pages/information";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/tasks" element={<Home />} />
+        <Route path="/new-task" element={<NewTask />} />
       </Routes>
     </Router>
   );
